38807a605a2abc0fd3285fb333d08c9f10caa613
“Tan_Cc” <739846601@qq.com>
2019-03-31 13:24:25 +0800

    init

---
+++ src/main/java/com/tancy/highconcurrent/HighconcurrentApplication.java
@@ -0,0 +1,13 @@
+package com.tancy.highconcurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighconcurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighconcurrentApplication.class, args);
+	}
+
+}
